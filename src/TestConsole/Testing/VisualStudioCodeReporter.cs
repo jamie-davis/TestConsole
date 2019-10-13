6d4b2e5f96c68df7f006f58157a19a372e745453
jamie-davis <jamie@clandavis.net>
2019-10-13 07:31:24 +0100

    Made Visual Studio Code reporter work on Ubuntu. On Windows, you need to start "code.cmd", on non-windows it's "code".

--- src/TestConsole/Testing/VisualStudioCodeReporter.cs
+++ src/TestConsole/Testing/VisualStudioCodeReporter.cs
@@ -1,9 +1,11 @@
+using System.Runtime.InteropServices;
+
 namespace TestConsoleLib.Testing
 {
     [ApprovalsReporter("code")]
     public class VisualStudioCodeReporter : IApprovalsReporter
     {
-        public string FileName => "code.cmd";
+        public string FileName => RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "code.cmd" : "code";
         public string Arguments => "--diff $1 $2";
     }
 }
