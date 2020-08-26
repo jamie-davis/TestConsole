7d6e6c4d6a22adae76061f103c3fa53c63d3aec5
Jamie Davis <jamie@clandavis.net>
2020-08-26 15:44:27 +0100

    Improved formatting of differences in the ApprovedFileMismatchException.

---
+++ src/TestConsole.Tests/Testing/TestApprovedFileMismatchException.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TestConsoleLib;
+using TestConsoleLib.Exceptions;
+using TestConsoleLib.Testing;
+
+namespace TestConsole.Tests.Testing
+{
+    [TestFixture]
+    public class TestApprovedFileMismatchException
+    {
+        [Test]
+        public void ExceptionMessageIsFormatted()
+        {
+            //Arrange
+            Exception exception = null;
+            var approvedText = @"Line 1
+Line 2";
+            var receivedText = @"Line 1
+Line 2 Diff";
+
+            using (var approved = WriteTempFile(approvedText))
+            using (var received = WriteTempFile(receivedText))
+            {
+                //Act
+                exception = new ApprovedFileMismatchException(received.TempPath, approved.TempPath);
+            }
+
+            //Assert
+            exception.Message.Verify();
+        }
+
+        [Test]
+        public void ExceptionMessageHandlesEmptyReceivedFile()
+        {
+            //Arrange
+            Exception exception = null;
+            var approvedText = @"Line 1
+Line 2";
+            var receivedText = string.Empty;
+
+            using (var approved = WriteTempFile(approvedText))
+            using (var received = WriteTempFile(receivedText))
+            {
+                //Act
+                exception = new ApprovedFileMismatchException(received.TempPath, approved.TempPath);
+            }
+
+            //Assert
+            exception.Message.Verify();
+        }
+
+        [Test]
+        public void ExceptionMessageHandlesEmptyApprovedFile()
+        {
+            //Arrange
+            Exception exception = null;
+            var approvedText  = string.Empty;
+            var receivedText= @"Line 1
+Line 2";
+
+            using (var approved = WriteTempFile(approvedText))
+            using (var received = WriteTempFile(receivedText))
+            {
+                //Act
+                exception = new ApprovedFileMismatchException(received.TempPath, approved.TempPath);
+            }
+
+            //Assert
+            exception.Message.Verify();
+        }
+
+        [Test]
+        public void ExceptionMessageHandlesIdenticalData()
+        {
+            //Arrange
+            Exception exception = null;
+            var approvedText  = string.Empty;
+
+            using (var approved = WriteTempFile(approvedText))
+            using (var received = WriteTempFile(approvedText))
+            {
+                //Act
+                exception = new ApprovedFileMismatchException(received.TempPath, approved.TempPath);
+            }
+
+            //Assert
+            exception.Message.Verify();
+        }
+
+        [Test]
+        public void MaximumDifferencesAreReported()
+        {
+            //Arrange
+            Exception exception = null;
+            var approvedText = string.Join(Environment.NewLine, Enumerable.Range(1, 100).Select(n => $"Line {n}"));
+            var receivedText = string.Join(Environment.NewLine, Enumerable.Range(1, 100).Select(n => $"Line {((n & 1) != 0 ? "X" : n.ToString())}"));
+
+            using (var approved = WriteTempFile(approvedText))
+            using (var received = WriteTempFile(receivedText))
+            {
+                //Act
+                exception = new ApprovedFileMismatchException(received.TempPath, approved.TempPath);
+            }
+
+            //Assert
+            exception.Message.Verify();
+        }
+
+
+        #region Temporary file handling
+
+        private TempFile WriteTempFile(string text)
+        {
+            return new TempFile(text);
+        }
+
+        private class TempFile : IDisposable
+        {
+            public string TempPath { get; }
+
+            public TempFile(string text)
+            {
+                TempPath = Path.GetTempFileName();
+                try
+                {
+                    File.WriteAllText(TempPath, text);
+                }
+                catch
+                {
+                    Dispose(); //We will not complete construction, don't leave the temp file in place
+                    throw;
+                }
+            }
+
+            #region IDisposable
+
+            public void Dispose()
+            {
+                File.Delete(TempPath);
+            }
+
+            #endregion
+        }
+
+        #endregion
+    }
+}
--- src/TestConsole/Exceptions/ApprovedFIleMismatchException.cs
+++ src/TestConsole/Exceptions/ApprovedFIleMismatchException.cs
@@ -1,13 +1,17 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
+using TestConsole.OutputFormatting;
 
 namespace TestConsoleLib.Exceptions
 {
     public class ApprovedFileMismatchException : Exception
     {
+        private const int MaxDifferences = 25;
+
         private static string[] _emptyLinesArray;
 
         public ApprovedFileMismatchException(string receivedFile, string approvedFile) : base(AnalyseFiles(receivedFile, approvedFile))
@@ -20,20 +24,33 @@ namespace TestConsoleLib.Exceptions
             var approvedLines = ReadFile(approvedFile);
 
             var common = Math.Min(receivedLines.Length, approvedLines.Length);
+            var longest = Math.Max(receivedLines.Length, approvedLines.Length);
 
-            var differences = receivedLines.Take(common).Zip(approvedLines.Take(common), (r, a) => new { Received = r, Approved = a})
-                .Select((l, ix) => new { Index = ix, l.Received, l.Approved })
+            var differenceLines = receivedLines.Take(common).Zip(approvedLines.Take(common), (r, a) => new { Received = r, Approved = a})
+                .Select((l, ix) => new { Index = ix, l.Received, l.Approved });
+
+            if (receivedLines.Length > common)
+            {
+                differenceLines = differenceLines.Concat(receivedLines.Skip(common).Select((l, ix) => new {Index = ix + common, Received = l, Approved = string.Empty}));
+            }
+
+            if (approvedLines.Length > common)
+            {
+                differenceLines = differenceLines.Concat(approvedLines.Skip(common).Select((l, ix) => new {Index = ix + common, Received = string.Empty, Approved = l}));
+            }
+
+            var differences = differenceLines
                 .Where(l => l.Received != l.Approved)
                 .ToList();
 
-            if (differences.Count == 0)
+            if (differences.Count == 0 && longest == common)
                 return "Files did not match, but no text differences detected.";
 
             try
             {
                 var output = new Output();
 
-                output.WrapLine("Differences found between approved and received:");
+                output.WrapLine($"{differences.Count} Differences found between approved and received:");
                 output.WriteLine();
 
                 output.FormatTable(new[]
@@ -43,11 +60,25 @@ namespace TestConsoleLib.Exceptions
                 });
 
                 output.WriteLine();
-                output.FormatTable(differences.SelectMany(d => new []
+                var diffRowLimit = MaxDifferences*2;
+                var diffLines = differences.SelectMany(d => new []
                 {
-                    new { File = "Approved", Line = d.Index + 1, Text = d.Approved },
-                    new { File = "Received", Line = d.Index + 1, Text = d.Received },
-                }));
+                    new { File = "Approved", Line = (int?)d.Index + 1, Text = d.Approved },
+                    new { File = "Received", Line = (int?)d.Index + 1, Text = d.Received },
+                }).Take(diffRowLimit);
+
+                if (MaxDifferences < differences.Count)
+                {
+                    diffLines = diffLines.Concat(new [] { new { File = "...", Line = (int?)null, Text = string.Empty } });
+                }
+
+                var report = diffLines.AsReport(rep => rep.RemoveBufferLimit()
+                    .AddColumn(d => d.File)
+                    .AddColumn(d => d.Line, cc => cc.RightAlign())
+                    .AddColumn(d => d.Text)
+                );
+
+                output.FormatTable(report);
 
                 return output.Report;
             }
